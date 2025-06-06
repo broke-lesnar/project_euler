73dec7a848f6b4e241d8a862b727a3e203b7457a
Vinod <vinodkumawat.notioninfosoft@gmail.com>
2025-06-06 14:44:29 +0530

    slight improvement in is_prime

--- solutions/is_prime.py
+++ solutions/is_prime.py
@@ -1,11 +1,12 @@
 from functools import lru_cache
+from math import sqrt, ceil
 
 
 @lru_cache(maxsize=None)
 def is_prime(n: int) -> bool:
     if n <= 1:
         return False
-    for i in range(2, n):
+    for i in range(2, ceil(sqrt(n)) + 1):
         if n % i == 0:
             return False
     return True
@@ -14,3 +15,6 @@ def is_prime(n: int) -> bool:
 if __name__ == '__main__':
     print(is_prime(61))
     print(is_prime(5))
+    print(is_prime(2303804))
+    print(is_prime(600851475143))
+    print(is_prime(6857))
